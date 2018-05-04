78872fa312f41c3b5a7b71f38de26ea9a990df1d
Ramin.Sabet <ramin.sabet.s@gmail.com>
2018-05-04 13:28:05 -0400

    MongoDb Modified 3

--- src/server.ts
+++ src/server.ts
@@ -88,7 +88,7 @@ class Server {
   public config(): void {
 
     // const MONGO_URI: string = 'mongodb+srv://ramin_sabet:NmMnNmMn@gettingstarted-hgi96.mongodb.net/chalpa'
-    const MONGO_URI:string = 'mongodb://localhost:27017/chelpa';
+    const MONGO_URI: string = 'mongodb://127.0.0.1:27017/chelpa';
     // const MONGO_URI: string = 'mongodb://ramin_sabet:NmMnNmMn@gettingstarted-shard-00-00-hgi96.mongodb.net:27017,gettingstarted-shard-00-01-hgi96.mongodb.net:27017,gettingstarted-shard-00-02-hgi96.mongodb.net:27017/chelpa?ssl=true&replicaSet=GettingStarted-shard-0&authSource=admin';
     mongoose.connect(MONGO_URI || process.env.MONGODB_URI, (err) => {
       if (err) {
