c72c01a9c85cd5fe550374991bbb0a5ef4035825
Ramin.Sabet <ramin.sabet.s@gmail.com>
2018-05-04 10:46:21 -0400

    MongoDB upgraded

--- build/server.js
+++ build/server.js
@@ -66,7 +66,8 @@ var Server = /** @class */ (function () {
     }
     // application config
     Server.prototype.config = function () {
-        var MONGO_URI = 'mongodb://ramin_sabet:NmMnNmMn@gettingstarted-shard-00-00-hgi96.mongodb.net:27017,gettingstarted-shard-00-01-hgi96.mongodb.net:27017,gettingstarted-shard-00-02-hgi96.mongodb.net:27017/chelpa?ssl=true&replicaSet=GettingStarted-shard-0&authSource=admin';
+        var MONGO_URI = 'mongodb+srv://ramin_sabet:NmMnNmMn@gettingstarted-hgi96.mongodb.net/chalpa';
+        // const MONGO_URI: string = 'mongodb://ramin_sabet:NmMnNmMn@gettingstarted-shard-00-00-hgi96.mongodb.net:27017,gettingstarted-shard-00-01-hgi96.mongodb.net:27017,gettingstarted-shard-00-02-hgi96.mongodb.net:27017/chelpa?ssl=true&replicaSet=GettingStarted-shard-0&authSource=admin';
         mongoose.connect(MONGO_URI || process.env.MONGODB_URI, function (err) {
             if (err) {
                 console.log(err);
