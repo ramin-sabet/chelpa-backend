5ade01d0d5ed2cb9305b48f3f33e29d4743b0e1c
Ramin.Sabet <ramin.sabet.s@gmail.com>
2018-05-07 10:32:20 -0400

    MongoDb Modified 15

--- build/server.js
+++ build/server.js
@@ -66,7 +66,8 @@ var Server = /** @class */ (function () {
     }
     // application config
     Server.prototype.config = function () {
-        var urlOpenShift = '127.0.0.1:27017/' + process.env.OPENSHIFT_chelpa; // const MONGO_URI: string = 'mongodb+srv://ramin_sabet:NmMnNmMn@gettingstarted-hgi96.mongodb.net/chalpa'
+        var urlOpenShift = '127.0.0.1:27017/' + process.env.OPENSHIFT_chelpa;
+        // const MONGO_URI: string = 'mongodb+srv://ramin_sabet:NmMnNmMn@gettingstarted-hgi96.mongodb.net/chalpa'
         // const MONGO_URI: string = 'mongodb://127.0.0.1:27017/chelpa';
         var MONGO_URI = 'mongodb://ramin_sabet:NmMnNmMn@gettingstarted-shard-00-00-hgi96.mongodb.net:27017,gettingstarted-shard-00-01-hgi96.mongodb.net:27017,gettingstarted-shard-00-02-hgi96.mongodb.net:27017/chelpa?ssl=true&replicaSet=GettingStarted-shard-0&authSource=admin';
         if (process.env.OPENSHIFT_MONGO_URI) {
