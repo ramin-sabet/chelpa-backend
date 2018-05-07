bde6498fdf790d00014682808f065daf6f073c9b
Ramin.Sabet <ramin.sabet.s@gmail.com>
2018-05-07 19:10:10 -0400

    MongoDB OPENSHIFT test4

--- build/server.js
+++ build/server.js
@@ -66,12 +66,13 @@ var Server = /** @class */ (function () {
     }
     // application config
     Server.prototype.config = function () {
-        var urlOpenShift = '127.0.0.1:27017/' + process.env.APP_NAME;
+        var urlOpenShift = process.env.MONGODB_PORT_27017_TCP_ADDR + '/' + process.env.MONGODB_SERVICE_PORT;
         var MONGO_URI = 'mongodb+srv://ramin_sabet:NmMnNmMn@gettingstarted-hgi96.mongodb.net/chalpa';
         // const MONGO_URI: string = 'mongodb://127.0.0.1:27017/chelpa';
         // const MONGO_URI: string = 'mongodb://ramin_sabet:NmMnNmMn@gettingstarted-shard-00-00-hgi96.mongodb.net:27017,gettingstarted-shard-00-01-hgi96.mongodb.net:27017,gettingstarted-shard-00-02-hgi96.mongodb.net:27017/chelpa?ssl=true&replicaSet=GettingStarted-shard-0&authSource=admin';
         // console.log(process.env);
         console.log(process.env);
+        console.log(urlOpenShift);
         // if (process.env.MONGO_URI) {
         //   console.log("HII");
         //   urlOpenShift = process.env.MONGO_URI +
