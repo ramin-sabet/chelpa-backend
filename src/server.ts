edeb0f9b52287264ab1b5718bc998cbbf84efb37
Ramin.Sabet <ramin.sabet.s@gmail.com>
2018-05-07 18:48:32 -0400

    MongoDB OPENSHIFT test2

--- src/server.ts
+++ src/server.ts
@@ -88,9 +88,9 @@ class Server {
   public config(): void {
 
     let urlOpenShift = '127.0.0.1:27017/' + process.env.APP_NAME;
-    // const MONGO_URI: string = 'mongodb+srv://ramin_sabet:NmMnNmMn@gettingstarted-hgi96.mongodb.net/chalpa'
+    const MONGO_URI: string = 'mongodb+srv://ramin_sabet:NmMnNmMn@gettingstarted-hgi96.mongodb.net/chalpa'
     // const MONGO_URI: string = 'mongodb://127.0.0.1:27017/chelpa';
-    const MONGO_URI: string = 'mongodb://ramin_sabet:NmMnNmMn@gettingstarted-shard-00-00-hgi96.mongodb.net:27017,gettingstarted-shard-00-01-hgi96.mongodb.net:27017,gettingstarted-shard-00-02-hgi96.mongodb.net:27017/chelpa?ssl=true&replicaSet=GettingStarted-shard-0&authSource=admin';
+    // const MONGO_URI: string = 'mongodb://ramin_sabet:NmMnNmMn@gettingstarted-shard-00-00-hgi96.mongodb.net:27017,gettingstarted-shard-00-01-hgi96.mongodb.net:27017,gettingstarted-shard-00-02-hgi96.mongodb.net:27017/chelpa?ssl=true&replicaSet=GettingStarted-shard-0&authSource=admin';
     // console.log(process.env);
     // console.log(process.env);
     // if (process.env.MONGO_URI) {
