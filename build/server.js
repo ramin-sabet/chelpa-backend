7c7c02505979df1a63efddade51dca26cfc38726
Ramin.Sabet <ramin.sabet.s@gmail.com>
2018-05-10 11:44:36 -0400

    AWS 1

--- build/server.js
+++ build/server.js
@@ -67,10 +67,10 @@ var Server = /** @class */ (function () {
     // application config
     Server.prototype.config = function () {
         // let urlOpenShift = process.env.MONGODB_PORT_27017_TCP_ADDR + '/' + process.env.MONGODB_SERVICE_PORT;
-        var MONGO_URI = 'mongodb+srv://ramin_sabet:NmMnNmMn@gettingstarted-hgi96.mongodb.net/chalpa';
+        // const MONGO_URI: string = 'mongodb+srv://ramin_sabet:NmMnNmMn@gettingstarted-hgi96.mongodb.net/chalpa'
         //   ("mongodb://user:password@[IP adresss]:[port]/[databaseName]", { auth: { authdb: "admin" } }, () => { })
         // const MONGO_URI: string = 'mongodb://72.30.6.118:27017/chelpa';
-        //  const MONGO_URI: string = 'mongodb://ramin_sabet:NmMnNmMn@gettingstarted-shard-00-00-hgi96.mongodb.net:27017,gettingstarted-shard-00-01-hgi96.mongodb.net:27017,gettingstarted-shard-00-02-hgi96.mongodb.net:27017/chelpa?ssl=true&replicaSet=GettingStarted-shard-0&authSource=admin';
+        var MONGO_URI = 'mongodb://ramin_sabet:NmMnNmMn@gettingstarted-shard-00-00-hgi96.mongodb.net:27017,gettingstarted-shard-00-01-hgi96.mongodb.net:27017,gettingstarted-shard-00-02-hgi96.mongodb.net:27017/chelpa?ssl=true&replicaSet=GettingStarted-shard-0&authSource=admin';
         // // console.log(process.env);
         console.log(process.env);
         // var url = 'mongodb://127.0.0.1:27017/' + process.env.OPENSHIFT_APP_NAME;
